75300e5f283e4239b26373c0ba88097bee58e125
Andrii <andrijprihodko@gmail.com>
2020-06-08 12:53:49 +0300

    v 0.01

---
+++ Library/Source.cpp
@@ -0,0 +1,241 @@
+﻿# include <iostream>
+#include <string>
+using namespace std;
+//###################################################
+class Library;
+class Book
+{
+private:
+	string name;
+	string autor;
+	string publishing;
+	int year = 0;
+	float rating = 0;
+public:
+	Book()
+	{
+		name = "";
+		autor = "";
+		publishing = "";
+		year = 0;
+		rating = 0;
+	}
+	Book(string name, string publishing, string autor, int year, float rating)
+	{
+		this->name = name;
+		this->autor = autor;
+		this->publishing = publishing;
+		this->year = year;
+		this->rating = rating;
+	}
+	void Print()
+	{
+		cout << "Name: " << name << endl;
+		cout << "Autor: " << autor << endl;
+		cout << "Publishing: " << publishing << endl;
+		cout << "Year: " << year << endl;
+		cout << "Rating: " << rating << endl;
+	}
+
+	friend Library;
+
+};
+//###################################################
+class Library
+{
+private:
+	static const int size = 10;
+	Book arr[size];
+public:
+
+	Library(Book book)
+	{
+
+	}
+	Library& operator=(const Book& other)
+	{
+		for (int i = 0; i < size;)
+		{
+			if (arr[i].name == "")
+			{
+				arr[i].name = other.name;
+				arr[i].autor = other.autor;
+				arr[i].publishing = other.publishing;
+				arr[i].year = other.year;
+				arr[i].rating = other.rating;
+				break;
+			}
+			else
+			{
+				i++;
+			}
+
+		}
+		return *this;
+	}
+	void Print()
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (arr[i].name != "")
+			{
+
+				cout << "Name: " << arr[i].name << endl;
+				cout << "Autor: " << arr[i].autor << endl;
+				cout << "Publishing: " << arr[i].publishing << endl;
+				cout << "Year: " << arr[i].year << endl;
+				cout << "Rating: " << arr[i].rating<<"/10" << endl << endl;
+			}
+		}
+	}
+	void PrintForSearch(int gfg)
+	{
+		cout << "Name: " << arr[gfg].name << endl;
+		cout << "Autor: " << arr[gfg].autor << endl;
+		cout << "Publishing: " << arr[gfg].publishing << endl;
+		cout << "Year: " << arr[gfg].year << endl;
+		cout << "Rating: " << arr[gfg].rating << "/10" << endl;
+	}
+	void SearchName(string g)
+	{
+		cout << endl << endl;
+		for (int i = 0; i < size; i++)
+		{
+			if (arr[i].name.find(g) != string::npos)
+			{
+				PrintForSearch(i);
+			}
+		}
+	}
+
+	void SearchAutor(string g)
+	{
+		cout << endl << endl;
+		for (int i = 0; i < size; i++)
+		{
+			if (arr[i].autor.find(g) != string::npos)
+			{
+				PrintForSearch(i);
+			}
+		}
+	}
+
+	void SearchPublishing(string g)
+	{
+		cout << endl << endl;
+		for (int i = 0; i < size; i++)
+		{
+			if (arr[i].publishing.find(g) != string::npos)
+			{
+				PrintForSearch(i);
+			}
+		}
+	}
+	void Sort()
+	{
+		Book temp;
+		for (int i = 1; i < size; i++)
+		{
+			for (int j = 1; j < size - i - 1; j++)
+			{
+				if (arr[j - 1].rating < arr[j].rating)
+				{
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+	void SortYear()
+	{
+		Book temp;
+		for (int i = 1; i < size; i++)
+		{
+			for (int j = 1; j < size - i - 1; j++)
+			{
+				if (arr[j - 1].year < arr[j].year)
+				{
+					temp = arr[j - 1];
+					arr[j - 1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+	void SearchRating(float a)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (arr[i].rating == a)
+			{
+				cout << "\n\t~~~~~Resut search rating~~~~~" << endl;
+				PrintForSearch(i);
+			}
+		}
+	}
+	
+	void SearchYear(int a)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (arr[i].year == a)
+			{
+				cout << "\n\t~~~~~Resut search year~~~~~" << endl;
+				PrintForSearch(i);
+			}
+		}
+		//###############################################
+	}
+	char mass[52] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z',
+					  'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n','o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x','y', 'z' };
+	void SortAlp()
+	{
+		char a = ' ';
+		for (int i = 0; i < size; i++)
+		{
+			a = arr[i].name.c_str()[0];
+			for (int j = 0; j < 52; j++)
+			{
+				if (a == mass[j])
+				{
+					cout << arr[i].name << endl;
+					break; break;
+				}
+
+			}
+		}
+
+	}
+};
+//###################################################
+int main()
+{
+	
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+
+	Book b1("Dead Dynasty", "Olga", "Oreo", 1999, 10);
+	Book b2("Formula life ", "Kate", "Oreo", 2008, 8);
+	Book b3("Dead Note", "Oleg", "Wno", 1329, 9);
+	b1.Print();
+	Library lib(b2);
+	lib.operator=(b1);
+	lib.operator=(b2);
+	lib.operator=(b3);
+	/*lib.Print();
+	lib.SearchRating(9);
+	lib.SearchName("Dead Dyn");*/
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	lib.Sort();
+	lib.Print();
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	lib.SortYear();
+	lib.Print();
+	cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+	lib.SortAlp();
+
+
+
+	system("pause");
+	return 0;
+}
